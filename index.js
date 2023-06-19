bcf09ad2322b38ccced02280c2fbabf1e204538c
Leeous <contact@leeous.com>
2023-06-19 15:57:50 -0400

    Add express server files, add node, add media queries

---
+++ index.js
@@ -0,0 +1,49 @@
+// Leeous.com server
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 443;
+const helmet = require("helmet");
+const path = require("path");
+const cors = require("cors");
+const serveIndex = require("serve-index");
+
+const corsOptions = {
+  origin: 'https://leeous.com',
+}
+
+app.use(cors());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/ftp', express.static('ftp'), serveIndex('ftp', {'icons': true}));
+
+// Serve public directory
+app.use(express.static('public'));
+
+// Basic routing  
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.get("/api/projects", (req, res) => {
+    res.sendFile(__dirname + `resource/js/data.json`);
+});
