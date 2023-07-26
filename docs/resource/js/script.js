a3a9437790bdcc610b56e405e5760be5a7c29acd
Cody Fields <contact@leeous.com>
2023-07-25 21:04:19 -0400

    Add "one shot" to projects, remove junk, reorg JS

--- docs/resource/js/script.js
+++ docs/resource/js/script.js
@@ -13,9 +13,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const lastUpdated = document.getElementById('pageLastUpdated');
   let projects = new Object();
 
-  // Last update to website
-  fetch('https://api.github.com/repos/Leeous/Leeous.github.io/commits')
-    .then(res => {
+  // Display last update to website in footer
+  fetch('https://api.github.com/repos/Leeous/Leeous.github.io/commits').then(res => {
       if (!res.ok) {
         throw new Error();
       } else {
@@ -23,23 +22,9 @@ window.addEventListener('DOMContentLoaded', () => {
         return res.json();
       }
     }).then(data => {
-      lastUpdated.innerText = `page last updated ${moment(data[0]['commit']['author']['date']).startOf('day').fromNow()}`; 
-    })
-    .catch( error => {
+      lastUpdated.innerText = `page last updated ${moment(data[0]['commit']['author']['date']).startOf('hour').fromNow()}`; 
+    }).catch( error => {
       console.log(error);
-    });
-
-  // Fetch repo API data
-  fetch('https://api.github.com/users/Leeous')
-  //Convert the response to a JSON object
-    .then(response => response.json()) 
-    .then( data => document.body.append())
-		.catch( error => {
-      if (!response.ok) {
-        console.error('Something went wrong!\n' + error);
-      } else {
-        console.log("Retrived repo API data!")
-      }
     });
 
   // Projects request
@@ -53,6 +38,25 @@ window.addEventListener('DOMContentLoaded', () => {
       console.log(error);
     });
 
+  // Parses the JSON file with project data
+  function populateProjects(projectData) {
+    Object.keys(projectData).forEach(function (key) {
+      // ${projectData[key].source ? `<button class='padding-small source open_project' data-link='${projectData[key].source}'>Source</button>` : ""}
+      // ${projectData[key].github ? `<button class='padding-small github open_project' data-link='${projectData[key].github}'>GitHub</button>` : ""}
+      // ${projectData[key].steam ? `<button class='padding-small steam open_project' data-link='${projectData[key].steam}'>Steam</button>` : ""}
+      const projectTemplate = `
+        <div class="project" projectData-id="${projectData[key].id}">
+        <h3 class="project_title">${projectData[key].name}</h3>
+        <!--<p class="project_last_update">updated <i>102 days ago</i></p>-->
+        <p class="project_desc">${projectData[key].desc}</p>
+        <div class="project_links">${projectData[key].links.source ? projectData[key].links.source : ''}${projectData[key].links.github ? projectData[key].links.github : ''}${projectData[key].links.steam ? projectData[key].links.steam : ''}</div>
+        </div>
+      `;
+      document.querySelector('#index_main #projects').insertAdjacentHTML('afterbegin', projectTemplate);
+      // TODO: Add detailed animation to display projects details
+    });
+  }
+
   // Navagation bar
   const navElements = document.querySelectorAll('.nav_link');
   if (!window.location.hash || !safeHashArray.includes(window.location.hash)) {
@@ -63,12 +67,7 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector(`[data-fadein="${window.location.hash}"`).classList.add('active_page');
   }
 
-  // window.onhashchange = () => {
-  //   let currentHash = (window.location.hash).substring("#");
-  //   console.log(currentHash)
-  //   fadeOut(document.querySelector('.active_page'), null, 10);
-  //   fadeIn(document.querySelector(currentHash));
-  // }
+  // Handles class switching for the navagation bar
   navElements.forEach((key) => {
     key.addEventListener('click', () => {
       clickSafe = false;
@@ -83,33 +82,16 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-
   // Add event listener for link click
   document.querySelectorAll('.open_project').forEach((e) => { e.addEventListener('click', () => { window.open(e.dataset.link) })});
+  
+  // Checks to see if hash tag is valid
+  if (!window.location.hash || !safeHashArray.includes(window.location.hash)) {
+    fadeIn('#about', null, 10); // Fade in initnal page - unless there's an anchor tag
+    document.querySelector('[data-fadein="#about"').classList.add('active_page');
+  } else {
+    fadeIn(window.location.hash, null, 10); // Fade in initnal page - unless there's an anchor tag
+    document.querySelector(`[data-fadein="${window.location.hash}"`).classList.add('active_page');
+  }
+
 });
-
-function populateProjects(projectData) {
-  Object.keys(projectData).forEach(function (key) {
-    // ${projectData[key].source ? `<button class='padding-small source open_project' data-link='${projectData[key].source}'>Source</button>` : ""}
-    // ${projectData[key].github ? `<button class='padding-small github open_project' data-link='${projectData[key].github}'>GitHub</button>` : ""}
-    // ${projectData[key].steam ? `<button class='padding-small steam open_project' data-link='${projectData[key].steam}'>Steam</button>` : ""}
-    const projectTemplate = `
-    <div class="project" projectData-id="${projectData[key].id}">
-    <h3 class="project_title">${projectData[key].name}</h3>
-    <!--<p class="project_last_update">updated <i>102 days ago</i></p>-->
-    <p class="project_desc">${projectData[key].desc}</p>
-    <div class="project_links">${projectData[key].links.source ? projectData[key].links.source : ''}${projectData[key].links.github ? projectData[key].links.github : ''}${projectData[key].links.steam ? projectData[key].links.steam : ''}</div>
-    </div>
-    `;
-    document.querySelector('#index_main #projects').insertAdjacentHTML('afterbegin', projectTemplate);
-    // TODO: Add detailed animation to display product details
-  });
-}
-
-if (!window.location.hash || !safeHashArray.includes(window.location.hash)) {
-  fadeIn('#about', null, 10); // Fade in initnal page - unless there's an anchor tag
-  document.querySelector('[data-fadein="#about"').classList.add('active_page');
-} else {
-  fadeIn(window.location.hash, null, 10); // Fade in initnal page - unless there's an anchor tag
-  document.querySelector(`[data-fadein="${window.location.hash}"`).classList.add('active_page');
-}
