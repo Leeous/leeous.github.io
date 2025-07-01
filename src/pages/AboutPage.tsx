b76a59f9fbea41ff5c7ff3a7ec2f4a5188a5af52
Cody Fields (Leeous) <contact@leeous.com>
2025-07-01 15:57:03 -0400

    add socials, add skills section

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,9 +3,23 @@ import PFP from '../assets/pfp.jpg';
 export default function Home() {
   return(
     <main className='about-page page'>
-      <h3>Cody Fields</h3>
-      <h4>North Carolina, USA</h4>
-      <img src={PFP} className='pfp' alt="A photo of Cody sitting in the woods." />
+      <section className='about'>
+        <h1 className='name'>Cody Fields</h1>
+        <h4 className='location'>North Carolina, USA</h4>
+        <img src={PFP} className='pfp' alt="A photo of Cody sitting in the woods." />
+      </section>
+      <section className='about-socials'>
+        <ul>
+          <li><a href="#">Buy me a coffee</a></li>
+          <li><a href="#">Linkedin</a></li>
+          <li><a href="#">Github</a></li>
+          <li><a href="#">Email</a></li>
+          <li><a href="#">PGP Key</a></li>
+        </ul>
+      </section>
+      <section>
+        <h2>Skills</h2>
+      </section>
     </main>
   )
 }
