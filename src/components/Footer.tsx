8e33386bccc2647452bc31049599be51ce403175
Cody Fields (Leeous) <contact@leeous.com>
2025-07-02 20:57:08 -0400

    add last commit to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
 export default function Footer() {
+  const [lastUpdate, setLastUpdate] = useState<string[]>([]);
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('https://api.github.com/repos/Leeous/Leeous.github.io/commits')
+      .then(response => response.json())
+      .then(data => {
+        const timeSinceLastUpdate = dayjs(data[0]['commit']['author']['date']).startOf('second').fromNow();
+        const lastCommitMessage = data[0]['commit']['message'];
+        setLastUpdate([timeSinceLastUpdate, lastCommitMessage]);
+      });
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, [lastUpdate]);
   return (
     <footer className="main-footer">
-      <p>Powered by coffee <span>&#9749;</span></p>
+      <p><span id="poweredBy">Powered by coffee <span className="coffee">&#9749;</span></span></p>
+      {lastUpdate.length != 0 &&
+        <p className="footer-pageLastUpdated">
+          page last updated <a href="${data[0]['html_url']}" target="_blank">{lastUpdate[0]}</a><br />
+          <span className="update-desc">{lastUpdate[1]}</span>
+        </p>
+      }
     </footer>
   )
 }
