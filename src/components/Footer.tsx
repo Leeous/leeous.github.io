0a629ae4f2f8bcbbe496e6e609e49759c93d21e6
Cody Fields (Leeous) <contact@leeous.com>
2025-07-06 18:25:20 -0400

    add margin & color to Footer, fix last commit URL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,15 +13,17 @@ export default function Footer() {
       .then(data => {
         const timeSinceLastUpdate = dayjs(data[0]['commit']['author']['date']).startOf('second').fromNow()
         const lastCommitMessage = data[0]['commit']['message']
-        setLastUpdate([timeSinceLastUpdate, lastCommitMessage])
+        const commitURL = data[0]['html_url'];
+        console.log(data)
+        setLastUpdate([timeSinceLastUpdate, lastCommitMessage, commitURL])
       })
-  }, [])
+    }, [])
   return (
     <footer className="main-footer">
-      <p><span id="poweredBy">Powered by coffee <span className="coffee">&#9749;</span></span></p>
+      <p><span id="powered-by">Powered by coffee <span className="coffee">&#9749;</span></span></p>
       {lastUpdate.length != 0 &&
-        <p className="footer-pageLastUpdated">
-          page last updated <a href="${data[0]['html_url']}" target="_blank">{lastUpdate[0]}</a><br />
+        <p className="last-commit">
+          page last updated <a href={lastUpdate[2]} target="_blank">{lastUpdate[0]}</a><br />
           <span className="update-desc">{lastUpdate[1]}</span>
         </p>
       }
