d2eacacde1581d93dc873b375f82b9ddf0509dfc
Cody Fields (Leeous) <contact@leeous.com>
2025-07-16 22:56:50 -0400

    scroll user to top of change of blog page if navigated to by link

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,7 +1,12 @@
 import posts from "../assets/data/blog_data.json"
 import Post from "../components/Post";
+import { useLayoutEffect } from "react";
 
 export default function Blog() {
+  // Ensure user is scrolled to the top of screen if using <Link> to get to blog
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  })
   return (
     <main className='blog-page page'>
       {posts.map((post) => {
