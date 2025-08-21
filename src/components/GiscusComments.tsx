d37b6a08fb74542bdb9a970da21f5cd5747b5662
Cody Fields (Leeous) <contact@leeous.com>
2025-08-21 16:23:53 -0400

    adjust GiscusComments to handle both blog posts and project pages

--- src/components/GiscusComments.tsx
+++ src/components/GiscusComments.tsx
@@ -1,22 +1,40 @@
 // components/GiscusComment.tsx
 import Giscus from "@giscus/react";
 
-export default function GiscusComment({ number }: { number: number }) {
+export default function GiscusComments({ projectName, number }: { number?: number, projectName?: string }) {
+
+  // return (
+  //   <Giscus
+  //     id="comments"
+  //     repo="Leeous/leeous.github.io"
+  //     repoId="MDEwOlJlcG9zaXRvcnkxODMyODkyMzg"
+  //     category="Blog Posts"
+  //     categoryId="DIC_kwDOCuzFls4CtgKV"
+  //     mapping="number"
+  //     term={String(number)}
+  //     reactionsEnabled="1"
+  //     emitMetadata="0"
+  //     inputPosition="bottom"
+  //     theme="custom"
+  //     lang="en"
+  //     loading="lazy"
+  //   />
+  // );
+
   return (
     <Giscus
       id="comments"
       repo="Leeous/leeous.github.io"
       repoId="MDEwOlJlcG9zaXRvcnkxODMyODkyMzg"
-      category="Blog Posts"
-      categoryId="DIC_kwDOCuzFls4CtgKV"
-      mapping="number"
-      term={String(number)}
+      category={number ? "Blog Posts" : "Project Discussions"}
+      categoryId={number ? "DIC_kwDOCuzFls4CtgKV" : "DIC_kwDOCuzFls4CuXEP"}
+      mapping={number ? "number" : "specific"}
+      term={number ? String(number) : projectName}
       reactionsEnabled="1"
       emitMetadata="0"
       inputPosition="bottom"
       theme="custom"
       lang="en"
-      loading="lazy"
     />
   );
 }
